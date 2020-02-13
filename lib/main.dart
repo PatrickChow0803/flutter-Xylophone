fc7d629885420ccd3289ba7283e09f73d004f320
patrick <patrickchow0803@gmail.com>
2020-02-13 18:01:37 -0500

    Added multiple buttons to play the multiple different wav files.

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,68 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me!'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('Click Me!'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('Click Me!'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('Click Me!'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('Click Me!'),
+              ),
+              FlatButton(
+                color: Colors.indigo,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('Click Me!'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('Click Me!'),
+              ),
+            ],
           ),
         ),
       ),
